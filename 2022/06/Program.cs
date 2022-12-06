faa02189ee14067e125cf8f30b110b4a48e2ad80
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-12-06 03:00:29 -0300

    Solution for puzzle 06

--- 2022/06/Program.cs
+++ 2022/06/Program.cs
@@ -6,7 +6,20 @@ internal sealed class Program
 
     private static async Task Main()
     {
-        var input = await File.ReadAllLinesAsync(_inputLocation);
-        Console.WriteLine(input);
+        var input = await File.ReadAllTextAsync(_inputLocation);
+
+        Console.WriteLine($"First answer: {MarkerEndsAt(input, 4)}");
+        Console.WriteLine($"Second answer: {MarkerEndsAt(input, 14)}");
+    }
+
+    private static int MarkerEndsAt(string text, int markerLength)
+    {
+        for (var index = 0; index < text.Length; index++)
+        {
+            if (text.Substring(index, markerLength).Distinct().Count() == markerLength)
+                return index + markerLength;
+        }
+
+        return -1;
     }
 }
