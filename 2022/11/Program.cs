e653f1b8ac64d9dc26f1d93e482d80e786a3e9cb
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-12-10 23:56:51 -0300

    Setup for day 11

---
+++ 2022/11/Program.cs
@@ -0,0 +1,12 @@
+namespace _11;
+
+internal sealed class Program
+{
+    private readonly static string _inputLocation = Path.Combine("Data", "input.txt");
+
+    private static async Task Main()
+    {
+        var input = await File.ReadAllLinesAsync(_inputLocation);
+        Console.WriteLine(input);
+    }
+}
